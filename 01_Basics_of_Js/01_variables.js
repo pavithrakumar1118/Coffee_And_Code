15be9aa730b69a8acfed2f1ac869b43972ed1158
Pavithra Kumar D <84068975+pavithrakumar1118@users.noreply.github.com>
2024-04-26 01:17:54 +0000

    Basics of 1 and 2 done

---
+++ 01_Basics_of_Js/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"   /*Most preferred*/
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// Table is used for the multiple printing of the values in the table format of the respective variables
