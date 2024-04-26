ee89f394d5bf6fd6f20ef2bc6406936f6e462dbc
Pavithra Kumar D <84068975+pavithrakumar1118@users.noreply.github.com>
2024-04-26 08:55:11 +0000

    Done till Summary of the data types

---
+++ 01_Basics_of_Js/datatypes_Summary.js
@@ -0,0 +1,45 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// --------------------------------------------------------------------
+
+// Stack Memory and Heap memory
+
+// The Stack Memory is used by the primitive datatypes and gives out the copy the value as arguments (Call by Value)
+// The Heap Memory is used by the non - primitive data types and gives out the reference as the arguments from the heap memory which is (Call by Reference)
